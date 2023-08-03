98a5586afca4e18d8882e2a9d772470bf26d059f
nattakulmon <nattakulmon@gosoft.co.th>
2023-08-03 18:24:06 +0700

    add base di

---
+++ app/src/main/java/com/example/weather_forecast_test_repository/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.example.weather_forecast_test_repository.di
+
+import android.app.Application
+import com.example.weather_forecast_test_repository.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+
+class AppModule : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoin(this)
+    }
+    private fun initKoin(app: Application) {
+        startKoin {
+            androidContext(app)
+            listOf<Module>(
+
+            )
+        }
+    }
+}
