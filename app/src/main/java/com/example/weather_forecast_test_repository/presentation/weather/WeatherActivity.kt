7593998e56ad6c81fefcf04707da4c36f3d4821a
nattakulmon <nattakulmon@gosoft.co.th>
2023-08-04 19:04:54 +0700

    config weather activity

--- app/src/main/java/com/example/weather_forecast_test_repository/presentation/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/weather_forecast_test_repository/presentation/weather/WeatherActivity.kt
@@ -7,25 +7,25 @@ import android.text.TextUtils
 import android.util.DisplayMetrics
 import android.util.TypedValue
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.unit.dp
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.example.weather_forecast_test_repository.databinding.ActivityWeatherNowBinding
 import androidx.lifecycle.Observer
 import com.example.weather_forecast_test_repository.R
 import com.example.weather_forecast_test_repository.dataclass.WeatherMainStat
-import com.example.weather_forecast_test_repository.dataclass.WeatherResult
+import com.example.weather_forecast_test_repository.getTemperatureText
+import com.example.weather_forecast_test_repository.presentation.forecast.ForecastActivity
 import kotlin.math.roundToInt
 
 class WeatherActivity: AppCompatActivity() {
     companion object {
-        private const val KEY_CITY_NAME = "city"
+        const val KEY_CITY_NAME = "city"
         fun getPage(context: Context, city: String): Intent{
             val intent =  Intent(context, WeatherActivity::class.java)
             intent.putExtra(KEY_CITY_NAME, city)
             return intent
         }
     }
-    private val kelvinDiff: Double = 273.15
+
     private val viewModel: WeatherViewModel by viewModel()
     private lateinit var intentCity: String
     private lateinit var binding: ActivityWeatherNowBinding
@@ -41,7 +41,7 @@ class WeatherActivity: AppCompatActivity() {
     }
 
     private fun getExtraParams() {
-        intentCity = intent.getStringExtra(KEY_CITY_NAME)?:""
+        intentCity = intent.getStringExtra(KEY_CITY_NAME)?:"Bangkok"
     }
     private fun setView(){
         binding.searchBar.layoutSearchEditText.setText(intentCity)
@@ -59,6 +59,13 @@ class WeatherActivity: AppCompatActivity() {
                 binding.searchBar.layoutSearchEditText.text.toString()
             )
         }
+        binding.buttonFullDayForecast.setOnClickListener{
+            val intent = ForecastActivity.getPage(
+                this,
+                binding.searchBar.layoutSearchEditText.text.toString()
+            )
+            startActivity(intent)
+        }
     }
 
     private fun initViewModel() {
@@ -72,8 +79,9 @@ class WeatherActivity: AppCompatActivity() {
     }
 
     private fun setTemperatureView (data: WeatherMainStat) {
-        binding.textTemperatureActual.text = getTemperatureText(data.temp)
-        binding.textTemperatureFeeling.text = getTemperatureText(data.feels_like)
+        val state = binding.buttonTempToggle.isChecked
+        binding.textTemperatureActual.text = data.temp.getTemperatureText(this, state)
+        binding.textTemperatureFeeling.text = data.temp.getTemperatureText(this, state)
     }
 
     private fun setHumidityView (humid: Int) {
@@ -85,41 +93,10 @@ class WeatherActivity: AppCompatActivity() {
 
     private fun setCityName(city: String) {
         val metrics: DisplayMetrics = resources.displayMetrics
-        var dp: Float = 20f
-        if(city.length in 9..14) {
-            dp = 15f
-        } else if(city.length in 14..19){
-            dp = 12f
-        } else if (city.length > 19) {
-            dp = 10f
-        }
+        var dp: Float = 20f - city.length.div(2)
         val fpixels: Float = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, dp, metrics);
         binding.textCity.textSize = fpixels
         binding.textCity.text = city
-    }
-
-    private fun getTemperatureText (temp: Double): String {
-        return if (binding.buttonTempToggle.isChecked){
-            getString(
-                R.string.text_temperature,
-                toFahrenheit(temp),
-                "F"
-            )
-        } else {
-            getString(
-                R.string.text_temperature,
-                toCelsius(temp),
-                "C"
-            )
-        }
-    }
-
-    fun toCelsius (temp: Double): String {
-        return temp.minus(kelvinDiff).roundToInt().toString()
-    }
-    fun toFahrenheit (temp: Double):String {
-        val tempF: Double = (1.80 * (temp - kelvinDiff)) + 32.00
-        return tempF.roundToInt().toString()
     }
 
 }
